ad2a93888b0f7db3b50407c3d0eeeace13d20726
Massu <massu211.works@gmail.com>
2020-07-24 23:17:03 +0900

    [add] 温度測定できるようにした

--- measure.py
+++ measure.py
@@ -1,10 +1,38 @@
+import math
+import time
+import argparse
 import adc
 
-def measure():
-    mcp = adc.MCP3208()
-    ch = 0
-    res = mcp.get(ch)
-    print(f"{res} V")
+
+class Thermistor():
+    def __init__(self, *args, **kwargs):
+        self.parser = argparse.ArgumentParser()
+
+        self.parser.add_argument("-c", "--channel", action="store",
+                                 help="A/Dコンバータのチャンネル(0～7). Default: 0", default=0, type=int)
+        self.parser.add_argument("-vr", "--v-ref", action="store",
+                                 help="A/Dコンバータの基準電圧の値. Default: 3.3", default=3.3, type=float)
+        self.parser.add_argument("-vd", "--v-division-resistance", action="store",
+                                 help="サーミスタ側の分圧抵抗の値(kΩ). Default: 4.7", default=4.7, type=float)
+
+    def measure(self):
+        '''サーミスタの温度を測定
+        '''
+
+        self.args = self.parser.parse_args()
+        vref = self.args.v_ref
+        vdr = self.args.v_division_resistance
+        ch = self.args.channel
+
+        mcp = adc.MCP3208()
+        vout = mcp.get(ch)
+        r_ther = (vref * vdr) / vout - vdr  # サーミスタの抵抗値
+        var_b = 3452.9 * math.pow(r_ther, -0.012329)  # 可変にしたB定数
+        temp = var_b / math.log(r_ther * math.exp(var_b / (25+273.15)) / 10) - 273.15
+
+        print(f"ch{ch}: {temp} ℃")
+
 
 if __name__ == "__main__":
-    measure()
+    thermistor = Thermistor()
+    thermistor.measure()
