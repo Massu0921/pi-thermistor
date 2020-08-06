bd9bd8f07c25ca983fb31528ba95b39f4abad29c
Massu <massu211.works@gmail.com>
2020-08-06 11:55:03 +0900

    [add] 複数のchを一度に取得できるように

--- measure.py
+++ measure.py
@@ -9,11 +9,11 @@ class Thermistor():
         self.parser = argparse.ArgumentParser()
 
         self.parser.add_argument("-c", "--channel", action="store",
-                                 help="A/Dコンバータのチャンネル(0～7). Default: 0", default=0, type=int, choices=[0,1,2,3,4,5,6,7])
+                                 help="A/Dコンバータのチャンネル(0～7). Default: 0", default=0, type=int, choices=[0,1,2,3,4,5,6,7], nargs='*')
         self.parser.add_argument("-v", "--v-ref", action="store",
                                  help="A/Dコンバータの基準電圧の値. Default: 3.3", default=3.3, type=float)
         self.parser.add_argument("-r", "--v-division-resistance", action="store",
-                                 help="サーミスタ側の分圧抵抗の値(kΩ). Default: 4.7", default=4.7, type=float)
+                                 help="サーミスタ側の分圧抵抗の値(kΩ). Default: 4.7", default=4.7, type=float, nargs='*')
 
     def measure(self):
         '''サーミスタの温度を測定
@@ -21,20 +21,24 @@ class Thermistor():
 
         self.args = self.parser.parse_args()
         vref = self.args.v_ref
-        vdr = self.args.v_division_resistance
-        ch = self.args.channel
+        vdrs = self.args.v_division_resistance
+        chs = self.args.channel
 
-        mcp = adc.MCP3208()
-        vout = mcp.get(ch)
+        for i in range(len(chs)):
+            ch = chs[i]
+            vdr = vdrs[i]
 
-        try:
-            r_ther = (vref * vdr) / vout - vdr  # サーミスタの抵抗値
-            var_b = 3452.9 * math.pow(r_ther, -0.012329)  # 可変にしたB定数
-            temp = var_b / math.log(r_ther * math.exp(var_b / (25+273.15)) / 10) - 273.15
+            mcp = adc.MCP3208()
+            vout = mcp.get(ch)
 
-            print(f"ch{ch}: {temp} ℃")
-        except ZeroDivisionError:
-            print("'NaN'")
+            try:
+                r_ther = (vref * vdr) / vout - vdr  # サーミスタの抵抗値
+                var_b = 3452.9 * math.pow(r_ther, -0.012329)  # 可変にしたB定数
+                temp = var_b / math.log(r_ther * math.exp(var_b / (25+273.15)) / 10) - 273.15
+
+                print(f"ch{ch}: {temp} ℃")
+            except ZeroDivisionError:
+                print("'NaN'")
 
 if __name__ == "__main__":
     thermistor = Thermistor()
